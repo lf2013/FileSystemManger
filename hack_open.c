8cfbdeb0245b4fcb79a9c5c9f73a7d45e492c1a3
gwcai <lili14520@gmail.com>
2013-04-27 15:58:34 +0800

    reconstruct

--- hack_open.c
+++ hack_open.c
@@ -1,6 +1,10 @@
 #include<linux/module.h>
 #include<linux/kernel.h>
 #include<linux/mm.h>
+#include<linux/init.h>
+#include<linux/syscalls.h>
+#include<asm/unistd.h>
+
 
 #include"my_sys_call.h"
 #include"get_call_table.h"
@@ -8,26 +12,74 @@
 MODULE_LICENSE("GPL");
 
 void **my_call_table;//存放系统调用表
+unsigned int orig_cr0;
 
+/*clear up WP bit of CR0*/
+unsigned int clear_and_return_cr0(void)
+{
+	unsigned int cr0 = 0;
+	unsigned int ret;
+	asm volatile("movl %%cr0,%%eax"
+			: "=a"(cr0)
+		     );
+	ret = cr0;
+	cr0 &= 0xfffeffff;//clear the 16 bit of CR0
+	asm volatile("movl %%eax,%%cr0"
+			:
+			: "a"(cr0)
+		    );
+	return ret;
+}
+/*set cr0 with new value*/
+void setback_cr0(unsigned val)
+{
+	asm volatile("movl %%eax,%%cr0"
+			:
+			: "a"(val)
+		    );
+}
+/*替换函数*/
+static int intercept_init(void)
+{
+	my_call_table = (void **)get_call_table();
+        if(my_call_table == NULL)
+                return -1;
+        else printk("sys_call_table\t%x\n",(unsigned)my_call_table);
+	
+	#define REPLACE(x) orig_##x = my_call_table[__NR_##x];\
+		my_call_table[__NR_##x] = my_##x
+
+	REPLACE(open); //替换open函数
+	REPLACE(write);//替换write函数
+	REPLACE(creat);//替换creat函数
+	REPLACE(unlink);//替换remove函数
+	return 0;
+}
 // 模块载入时被调用
-
 static int __init init_my_module(void)
 
 {
-	my_call_table =	(void **)get_call_table();
-	if(my_call_table == NULL)
-		return -1;
-	else printk("sys_call_table\t%X\n",(unsigned)my_call_table);
-	return 0;
+	int ret;
+	orig_cr0 = clear_and_return_cr0();
+	ret = intercept_init();
+	setback_cr0(orig_cr0);
+	return ret;
 }
 
 
 
-// 模块卸载时被调用
-
+/* 模块卸载时被调用，主要是还原系统调用*/
 static void __exit clean_my_module(void)
 {
+	#define RESTORE(x) my_call_table[__NR_##x] = orig_##x
+
+	orig_cr0 = clear_and_return_cr0();   
+	RESTORE(open);
+	RESTORE(write);
+	RESTORE(unlink);
+	RESTORE(creat);
+	setback_cr0(orig_cr0);
 }
 
-module_init(init_my_module);
-module_exit(clean_my_module);
+module_init(init_my_module); //初始化模块
+module_exit(clean_my_module); //卸载模块
--- my_sys_call.h
+++ my_sys_call.h
@@ -1,3 +1,43 @@
 #include<linux/user.h>
-asmlinkage int (*orig_write)(unsigned int fd,char *buf,unsigned int count);
-asmlinkage long (*orig_open)(char __user *filename,int flags,int mode);
+#include<linux/types.h>
+#include<linux/fcntl.h>
+
+/******************************原始系统调用函数表******************************/
+
+asmlinkage size_t (*orig_write)(int fd,const void *buf,size_t count);
+asmlinkage long (*orig_open)(char __user *filename,int flags,mode_t mode);
+asmlinkage ssize_t (*orig_read)(int fd,void *buf,size_t count);
+asmlinkage int (*orig_creat)(const char *filename,mode_t mode);
+asmlinkage int (*orig_unlink)(const char *filename);
+
+/******************************自定义系统调用函数表****************************/
+
+asmlinkage long my_open(char __user *filename,int flags,mode_t mode)
+{
+        printk("call open()\n");
+	if(filename != NULL)
+	        printk("%s was opened!\n",filename);
+        return orig_open(filename,flags,mode);
+}
+asmlinkage size_t my_write(int fd,const void *buf,size_t count)
+{
+	printk("call write()\n");
+	if(count != 0)
+		printk("向文件 %d 写入 %d 字节\n",fd,count);
+	return orig_write(fd,buf,count);
+}
+asmlinkage int my_creat(const char *filename,mode_t mode)
+{
+	 printk("call creat()\n");
+	if(filename != NULL)
+       		 printk("file %s was created!\n",filename);
+        return orig_creat(filename,mode);
+
+}
+asmlinkage int my_unlink(const char *filename)
+{
+	 printk("call unlink()\n");
+	if(filename != NULL)
+       		 printk("file %s was removed!\n",filename);
+	return orig_unlink(filename);
+}
